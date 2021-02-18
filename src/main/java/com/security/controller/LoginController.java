996e854bb54c3aa3eff3d502ca91e167b2cec306
김현진 <hyeonjin.kim@innowireless.com>
2021-02-18 18:19:31 +0900

    spring security demo upload

---
+++ src/main/java/com/security/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.security.controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+    final private static Logger LOG = Logger.getGlobal();
+    public static final String SECURED_TEXT = "Hello from the secured resource!";
+
+    @GetMapping("/login")
+    public void login() {
+        LOG.info("GET successfully called on /login resource");
+    }
+
+    @GetMapping("/hello")
+    public void hello() {
+        System.out.println("HelloWorld");
+    }
+}
